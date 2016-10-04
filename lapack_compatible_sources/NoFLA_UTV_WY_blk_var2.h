118fb03e3e4e5a27f160ff6a4c05a7adf7ec1b56
gquintanaorti <gquintan@icc.uji.es>
2016-10-04 20:37:21 +0200

    Add files via upload

---
+++ lapack_compatible_sources/NoFLA_UTV_WY_blk_var2.h
@@ -0,0 +1,7 @@
+
+int NoFLA_UTV_WY_blk_var2(
+        int m_A, int n_A, double * buff_A, int ldim_A,
+        int build_u, int m_U, int n_U, double * buff_U, int ldim_U,
+        int build_v, int m_V, int n_V, double * buff_V, int ldim_V,
+        int nb_alg, int pp, int n_iter );
+
