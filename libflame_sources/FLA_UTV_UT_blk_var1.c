5b654e4b36beb045ce8875f3869b1bf20e88f652
gquintanaorti <gquintan@icc.uji.es>
2016-10-05 12:21:23 +0200

    Add files via upload

--- libflame_sources/FLA_UTV_UT_blk_var1.c
+++ libflame_sources/FLA_UTV_UT_blk_var1.c
@@ -97,8 +97,8 @@ static FLA_Error MyFLA_QRP_update_partial_norms_opt( int m_A, int n_A,
 
 
 // ============================================================================
-int FLA_UTV_UT_blk_var1( FLA_Obj A, int build_u, FLA_Obj U, 
-        int build_v, FLA_Obj V, int nb_alg, int pp, int n_iter ) {
+FLA_Error FLA_UTV_UT_blk_var1( FLA_Obj A, int build_u, FLA_Obj U, 
+              int build_v, FLA_Obj V, int nb_alg, int pp, int n_iter ) {
 //
 // randUTV: It computes the UTV factorization of matrix A.
 //
